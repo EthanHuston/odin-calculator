6638ac3bb9597396f33292c28690af0e2c58268e
Ethan Huston <71285039+EthanHuston@users.noreply.github.com>
2025-06-25 12:17:09 -0400

    Completing base HTML structure and ID/Class names
    Filled out the index.html file with the current
    layout for the app along with ids and classes for
    elements.
    
    Additionally completed first functions for
    evaluating operations.

---
+++ script.js
@@ -0,0 +1,42 @@
+//Globals
+let operandOne = "";
+let operandTwo = "";
+let currentOperator = null;
+//UI Elements
+
+//Functions
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+
+    case "*":
+      return multiply(num1, num2);
+
+    case "/":
+      return divide(num1, num2);
+
+    default:
+      console.error("Unknown operation!");
+      break;
+  }
+}
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 + -num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
